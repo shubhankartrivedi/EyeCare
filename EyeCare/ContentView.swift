03564863a76c1111dcd7082efd2e90c8647d52e7
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-04-12 02:22:48 +0530

    Day1

--- EyeCare/ContentView.swift
+++ EyeCare/ContentView.swift
@@ -7,15 +7,80 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var timeRemaining = 60
+    @State private var isTimerRunning = false
+    @State private var restInterval = 60
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Rest your eyes!")
+                .font(.title)
+            
+            // Show the TimerView, which contains the progress bar
+            TimerView(timeRemaining: $timeRemaining, isTimerRunning: $isTimerRunning, restInterval: restInterval)
+                .padding(.vertical)
+            
+            Button(isTimerRunning ? "Stop" : "Start") {
+                isTimerRunning.toggle()
+                
+                
+                if isTimerRunning {
+                    // Start the timer
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        } else {
+                            // Time's up, reset the timer
+                            timeRemaining = restInterval
+                            isTimerRunning = false
+                            timer.invalidate()
+                        }
+                    }
+                } else {
+                    // Stop the timer
+                    isTimerRunning.toggle()
+                    timeRemaining = restInterval
+                }
+            }
+            
+            Picker("Rest Interval", selection: $restInterval) {
+                Text("30 seconds").tag(30)
+                Text("60 seconds").tag(60)
+                Text("90 seconds").tag(90)
+            }
+            .pickerStyle(SegmentedPickerStyle())
         }
         .padding()
+    }
+}
+struct TimerView: View {
+    @Binding var timeRemaining: Int
+    @Binding var isTimerRunning: Bool
+    let restInterval: Int
+    
+    var body: some View {
+        ProgressView(value: Double(restInterval - timeRemaining), total: Double(restInterval))
+            .onChange(of: isTimerRunning) { newValue in
+                if newValue {
+                    // Start the timer
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        } else {
+                            // Time's up, reset the timer
+                            
+                            timeRemaining = restInterval
+                            isTimerRunning = false
+                            timer.invalidate()
+                        }
+                    }
+                } else {
+                    // Stop the timer and reset the progress bar
+                    timeRemaining = restInterval
+                }
+            }
     }
 }
 
