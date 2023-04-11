d79c05a1d65f764b2e82cda4985afd43732497e3
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-04-12 00:41:01 +0530

    Initial Commit

---
+++ EyeCare/EyeCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeCareApp.swift
+//  EyeCare
+//
+//  Created by Shubhankar Trivedi on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EyeCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
