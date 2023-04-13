bcd15a78a29396156c7f1d06868f76a0cef31bb2
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-04-14 03:11:17 +0530

    WorkingModel

--- EyeCare/ContentView.swift
+++ EyeCare/ContentView.swift
@@ -1,91 +1,135 @@
-//
-//  ContentView.swift
-//  EyeCare
-//
-//  Created by Shubhankar Trivedi on 12/04/23.
-//
-
 import SwiftUI
+import UserNotifications
 
+
+struct ReminderApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 400, minHeight: 400)
+                .accentColor(.blue)
+        }
+        .windowStyle(HiddenTitleBarWindowStyle())
+        .commands {
+            CommandMenu("Application") {
+                Button("Quit", action: {
+                    NSApplication.shared.terminate(nil)
+                })
+                .keyboardShortcut("q", modifiers: [.command])
+            }
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
+    static let shared = AppDelegate()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        UNUserNotificationCenter.current().delegate = self
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { success, error in
+            if let error = error {
+                print("Error requesting authorization: \(error)")
+            } else {
+                print("Successfully requested authorization")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received notification: \(notification)")
+        completionHandler([.banner])
+    }
+}
 
 struct ContentView: View {
-    @State private var timeRemaining = 60
-    @State private var isTimerRunning = false
-    @State private var restInterval = 60
+    @State private var reminderTime = 1
+    @State private var isActive = false
+    @State private var timeRemaining = "00:00"
     
     var body: some View {
         VStack {
-            Text("Rest your eyes!")
-                .font(.title)
+            HStack {
+                Image(systemName: "eye.fill")
+                    .font(.title)
+                    .foregroundColor(.accentColor)
+                Text("Rest Your Eyes Reminder")
+                    .font(.title)
+            }
+            .padding(.bottom, 20)
             
-            // Show the TimerView, which contains the progress bar
-            TimerView(timeRemaining: $timeRemaining, isTimerRunning: $isTimerRunning, restInterval: restInterval)
-                .padding(.vertical)
-            
-            Button(isTimerRunning ? "Stop" : "Start") {
-                isTimerRunning.toggle()
-                
-                
-                if isTimerRunning {
-                    // Start the timer
-                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-                        if timeRemaining > 0 {
-                            timeRemaining -= 1
-                        } else {
-                            // Time's up, reset the timer
-                            timeRemaining = restInterval
-                            isTimerRunning = false
-                            timer.invalidate()
-                        }
-                    }
-                } else {
-                    // Stop the timer
-                    isTimerRunning.toggle()
-                    timeRemaining = restInterval
-                }
+            Stepper(value: $reminderTime, in: 1...120) {
+                Text("Remind me every \(reminderTime) minutes")
+                    .foregroundColor(.secondary)
             }
             
-            Picker("Rest Interval", selection: $restInterval) {
-                Text("30 seconds").tag(30)
-                Text("60 seconds").tag(60)
-                Text("90 seconds").tag(90)
+            Button(isActive ? "Stop" : "Start") {
+                isActive.toggle()
             }
-            .pickerStyle(SegmentedPickerStyle())
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .padding()
+            
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 10)
+                    .opacity(0.3)
+                    .foregroundColor(.accentColor)
+                
+                Text(timeRemaining)
+                    .font(.system(size: 50, weight: .thin, design: .rounded))
+                    
+            }
+            .frame(width: 200, height: 200)
+            .padding(.bottom, 30)
+            
+            
+            
+            
+            
+            Spacer()
         }
         .padding()
-    }
-}
-struct TimerView: View {
-    @Binding var timeRemaining: Int
-    @Binding var isTimerRunning: Bool
-    let restInterval: Int
-    
-    var body: some View {
-        ProgressView(value: Double(restInterval - timeRemaining), total: Double(restInterval))
-            .onChange(of: isTimerRunning) { newValue in
-                if newValue {
-                    // Start the timer
-                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-                        if timeRemaining > 0 {
-                            timeRemaining -= 1
-                        } else {
-                            // Time's up, reset the timer
+        .onReceive(
+            Timer.publish(every: 1, on: .main, in: .common).autoconnect(),
+            perform: { _ in
+                if isActive {
+                    let secondsRemaining = Int(reminderTime * 60) - Int(Date().timeIntervalSince1970
+                        .truncatingRemainder(dividingBy: Double(reminderTime * 60)))
+                        let minutes = secondsRemaining / 60
+                        let seconds = secondsRemaining % 60
+                        timeRemaining = String(format: "%02d:%02d", minutes, seconds)
+                        print(secondsRemaining)
+                        if secondsRemaining == 20 {
                             
-                            timeRemaining = restInterval
-                            isTimerRunning = false
-                            timer.invalidate()
+                            sendNotification()
                         }
+                    } else {
+                        timeRemaining = "00:00"
+                        
                     }
+                }
+            )
+        }
+        
+        private func sendNotification() {
+            let content = UNMutableNotificationContent()
+            content.title = "Rest Your Eyes"
+            content.body = "It's time to give your eyes a break!"
+            content.sound = UNNotificationSound.defaultCritical
+
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) { error in
+                if let error = error {
+                    print("Error sending notification: \(error)")
                 } else {
-                    // Stop the timer and reset the progress bar
-                    timeRemaining = restInterval
+                    print("Notification sent successfully")
                 }
             }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
